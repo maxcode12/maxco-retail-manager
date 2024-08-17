01e4ca20e3a072a211dfb217bd1ef89a1eb9f670
Maxwell Aruakpor <maxgudly@gmail.com>
2024-08-17 04:31:28 -0400

    created the layout format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SideNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/side-navbar/side-navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navbar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      side-navbar works!
+    </p>
+  `,
+  styleUrl: './side-navbar.component.scss'
+})
+export class SideNavbarComponent {
+
+}
