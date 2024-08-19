04199b994bf7e48727808740c7af66759458fa4a
Maxwell Aruakpor <maxgudly@gmail.com>
2024-08-19 03:52:50 -0400

    configure my route

--- src/app/pages/admin/dashboards/dashboards.component.ts
+++ src/app/pages/admin/dashboards/dashboards.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../../layout/header/header.component";
+import { SideNavbarComponent } from "../../../../layout/side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-dashboards',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SideNavbarComponent],
   templateUrl: './dashboards.component.html',
   styleUrl: './dashboards.component.scss'
 })
---
+++ src/services/constant/constants.ts
@@ -0,0 +1,12 @@
+import { METHODS } from "http";
+
+export const Constant = {
+    API_END_POINT: 'http://localhost:3000',
+    METHODS: {
+        GET_ALL_PRODUCT: 'GetAllProduct',
+        GET_ALL_CATEGORY: 'GetAllCategory',
+        POST: 'POST',
+        PUT: 'PUT',
+        DELETE: 'DELETE'
+    }
+}
